3dd2f13e538e3649a824cc72559632865e14957c
TMCreme <tonnybright123@yahoo.com>
2024-05-31 01:58:23 +0000

    added binary packages for spark

--- dags/main.py
+++ dags/main.py
@@ -141,7 +141,8 @@ with DAG(
             "spark.executor.heartbeatInterval": 100000,
             "spark.storage.blockManagerSlaveTimeoutMs": 100000,
             "spark.driver.maxResultSize": "10g"
-        }
+        },
+        packages="org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,com.datastax.spark:spark-cassandra-connector_2.12:2.5.1"
     )
 
     @task
